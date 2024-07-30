5f8253cd498e6b43cf6e1ed2e9e4e60122fc9cf6
Ayushi Jhinjhore <Ayushijhinjhore12@gmail.com>
2024-07-30 22:33:47 +0530

    done

--- components/CoinDisplay.js
+++ components/CoinDisplay.js
@@ -3,13 +3,34 @@ import CoinCard from "./CoinCard";
 
 function CoinDisplay({ coin }) {
   const [coins, setCoins] = useState([]);
+  
+  async function getTopCoinsInfo() {
+    try {
+      const response = await fetch('https://api.coinlore.net/api/tickers')
+      const coinsData = await response.json();
+      // console.log(coinsData);
+      setCoins(coinsData.data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getTopCoinsInfo();
+  },[])
 
   return (
     <div className="home">
       <h1>Top 20 Cryptos</h1>
       <div className="coins-container">
-        {coins.map((coin) => (
-          <CoinCard />
+        {coins.slice(0, 20).map(({id, name, symbol, rank, price_usd}) => (
+          <CoinCard 
+            id={id}
+            name={name}
+            symbol={symbol}
+            rank={rank}
+            price={price_usd}
+            key={id}
+          />
         ))}
       </div>
     </div>
